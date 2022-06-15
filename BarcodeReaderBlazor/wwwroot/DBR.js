8f5944f47427d42cff906af9928d5194c8ef9fc1
xulihang <xulihanghai@163.com>
2022-06-15 14:43:08 +0800

    init

---
+++ BarcodeReaderBlazor/wwwroot/DBR.js
@@ -0,0 +1,14 @@
+﻿let scanner;
+async function startScan(dotNetRef) {
+    if (!scanner) {
+        // specify a license, you can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=intro&product=dbr&package=js to get your own trial license good for 30 days. 
+        Dynamsoft.DBR.BarcodeScanner.license = 'DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9';
+        scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+    }
+    scanner.onUniqueRead = (txt, result) => {
+        console.log(result);
+        dotNetRef.invokeMethodAsync('onUniqueRead', txt);
+        scanner.hide();
+    };
+    await scanner.show();
+}
